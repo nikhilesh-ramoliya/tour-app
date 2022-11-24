85e0c1b76ce95ce7760001d47f93cb4126193782
niksPatel1 <ramoliyanikhil252525@gmail.com>
2022-11-24 21:18:36 +0530

    Revert "env change base url"
    This reverts commit f57bc07afa10b73c2c77aa4524daee7a2a7f5e1d.

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 
 API.interceptors.request.use((req) => {
